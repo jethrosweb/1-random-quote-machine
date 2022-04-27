4fe3f1c8591ac6b235b3a49f1fa57d3ba9ba02bf
Jethro Dean <jethroldean@gmail.com>
2022-04-27 13:29:10 +1000

    Getting random quotes from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from "react"
 import "./App.scss"
 
+const quotesUrl = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+
 export default function App() {
+    const [quote, setQuote] = React.useState("A house divided against itself cannot stand.")
+
+    const [author, setAuthor] = React.useState("Abraham Lincoln")
+
+    const [randomNumber, setRandomNumber] = React.useState(0)
+
+    const [quotesArray, setQuotesArray] = React.useState(null)
+
+    const fetchQuotes = async (url) => {
+        const response = await fetch(url)
+        const parsedJSON = await response.json()
+        setQuotesArray(parsedJSON.quotes)
+        console.log(parsedJSON)
+    }
+
+    React.useEffect(() => {
+        fetchQuotes(quotesUrl)
+    }, [quotesUrl])
+
+    const generateRandomQuote = () => {
+        const randomIndex = Math.floor(Math.random() * quotesArray.length)
+        setRandomNumber(randomIndex)
+        setQuote(quotesArray[randomIndex].quote)
+        setAuthor(quotesArray[randomIndex].author)
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Hello World!</h1>
+                <h1>Random Number: {randomNumber}</h1>
+                <p>"{quote}"</p>
+                <p>- {author}</p>
+                <button onClick={generateRandomQuote}>New Quote</button>
             </header>
         </div>
     )
